a071145a0816e382e3889709f809f48a75a09f71
Raul <raulrufino0@gmail.com>
2020-12-27 15:24:15 -0300

    adding example of a component using the Dialog HOC

---
+++ src/Examples/SampleExample.js
@@ -0,0 +1,30 @@
+import CreateDialog from '../Dialog/Dialog.js';
+
+
+function Top() {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+
+function Content() {
+  return (
+    <div>
+      <p>
+        Mussum Ipsum, cacilds vidis litro abertis. Paisis, filhis, espiritis santis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.
+      </p>
+      <p>
+Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Viva Forevis aptent taciti sociosqu ad litora torquent. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. 
+        </p>
+    </div>
+  )
+}
+
+
+const SampleExample = CreateDialog(Top, Content, 50)
+
+
+export default SampleExample

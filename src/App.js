0d79ed9b87231ac35a5abc5cb3607ca3d7e7da4a
Raul <raulrufino0@gmail.com>
2020-12-27 15:24:39 -0300

    importing initial example in the screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import SampleExample from './Examples/SampleExample.js'
+
 
 function App() {
   return (
     <div className="App">
+      <SampleExample/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

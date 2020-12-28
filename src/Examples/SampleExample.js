ca212298b9e8ce2c3e5d0459a2382c9e52243b7f
Raul <raulrufino0@gmail.com>
2020-12-28 16:50:06 -0300

    fixing css properties of first example

--- src/Examples/SampleExample.js
+++ src/Examples/SampleExample.js
@@ -2,23 +2,21 @@ import CreateDialog from '../Dialog/Dialog.js';
 
 
 function Top() {
-  return (
-    <div>
-      Hello
-    </div>
-  )
+  return <p 
+    style={{
+      "margin"  : "0px",
+      "padding" : "10px"
+    }}
+    > Mussum Ipsum </p>
 }
 
 
 function Content() {
   return (
-    <div>
+    <div style={{"backgroundColor" : "white"}}>
       <p>
         Mussum Ipsum, cacilds vidis litro abertis. Paisis, filhis, espiritis santis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.
       </p>
-      <p>
-Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Viva Forevis aptent taciti sociosqu ad litora torquent. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. 
-        </p>
     </div>
   )
 }
@@ -28,7 +26,6 @@ const SampleExample = CreateDialog({
   TopBar : Top,
   DialogBody : Content,
   defaultSizeInPercentage : 30,
-  dialogStyle : {"color" : "cyan"}
 })
 
 

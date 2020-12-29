b6af93949ba7b4358c78d2e42a280dc823a16b63
Raul <raulrufino0@gmail.com>
2020-12-29 08:42:18 -0300

    removing code that import the deleted files

--- src/App.js
+++ src/App.js
@@ -6,21 +6,9 @@ function App() {
   return (
     <div className="App">
       <SampleExample/>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
 
-export default App;
+
+export default App

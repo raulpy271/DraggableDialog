f0a06b950c9f193f673541591147f2f5288f4214
Raul <raulrufino0@gmail.com>
2020-12-31 16:42:38 -0300

    Adding methods and properties of Draggable

--- src/Draggable/Draggable.js
+++ src/Draggable/Draggable.js
@@ -1,7 +1,59 @@
 import DraggableWithoutTouchSupport from './DraggableWithoutTouchSupport.js'
 
 
-class Draggable extends DraggableWithoutTouchSupport {}
+class Draggable extends DraggableWithoutTouchSupport {
+
+
+  constructor({ 
+    dialogId,
+    elementThatCaptureThatClickId,
+    showButtonId,
+    hideButtonId
+  }) {
+    super({
+      dialogId,
+      elementThatCaptureThatClickId,
+      showButtonId,
+      hideButtonId
+    })
+
+
+    this.lastTouchX = 0
+    this.lastTouchY = 0
+    this.elementThatCaptureThatClick.ontouchstart = this.touch
+  }
+
+
+  updateLastTouch = touch => {
+    this.lastTouchX = touch.clientX
+    this.lastTouchY = touch.clientY
+  }
+
+
+  updatePositionWhenHoldTouch = touchEvent => {
+    if (touchEvent.touches.length === 1 ) {
+      let touch = touchEvent.touches[0]
+      let movementX = touch.clientX - this.lastTouchX
+      let movementY = touch.clientY - this.lastTouchY
+      this.updatePosition(movementX, movementY)
+      this.updateLastTouch(touch)
+    }
+  }
+
+
+  touch = touchEvent => {
+    let touch = touchEvent.touches[0]
+    window.ontouchmove = this.updatePositionWhenHoldTouch
+    window.ontouchend  = this.endTouch
+    this.updateLastTouch(touch)
+  }
+
+
+  endTouch = () => {
+    window.ontouchmove = null
+    window.ontouchend  = null
+  }
+}
 
 
 export default Draggable

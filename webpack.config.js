adde9b15329db2d0b80c629450b77b31b6587c35
Raul <raulrufino0@gmail.com>
2021-01-03 14:15:33 -0300

    changing name of the folder with the bundler

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,9 @@ const path = require('path')
 
 module.exports = {
   entry: './lib/Draggable.js',
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'Draggable.js',
     library: 'Draggable',
     libraryTarget: 'var'
